f39331afd785893ad28a92098548f49f1e6539f0
Alexander Sabanov <alexsab@mail.ru>
2019-10-11 20:55:15 +0400

    добавил карточку авто
    как на сайте infinitisamara

--- js/_custom.js
+++ js/_custom.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	// Custom JS
+	var $ = function(name) { return document.querySelector(name) },
+	$$ = function(name) { return document.querySelectorAll(name) };
+
+	$$('.kdx-ui-card__carousel-cursor-detector').forEach(function(element) {
+		element.addEventListener('click', function(t) {
+			
+        });
+	});
 
 });
+
+			
